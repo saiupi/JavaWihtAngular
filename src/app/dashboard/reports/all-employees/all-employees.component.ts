425428886a74f5e177a7ad485097e6e8ab64e96d
Saikumar Kasaboina <sk19306@corp.ojas-it.com>
2020-10-14 17:54:03 +0530

    angular poc with java

--- src/app/dashboard/reports/all-employees/all-employees.component.ts
+++ src/app/dashboard/reports/all-employees/all-employees.component.ts
@@ -14,38 +14,44 @@ export class AllEmployeesComponent implements OnInit {
   results: any;
   studentDetails: any = [];
   data: Object;
-  pageNo=0;
-  pageSize=5;
-
+  employeId: any;
+  save: any;
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private myRoute: Router) { }
+
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      id: ['', Validators.required],
-      name: ['', Validators.required],
-      sal: ['', Validators.required],
-      phone: ['', Validators.required],
-      city: ['', Validators.required]
+      empId: ['', Validators.required],
+      empName: ['', Validators.required],
+      empSal: ['', Validators.required],
+      mobileNumber: ['', Validators.required],
+      addres: ['', Validators.required],
 
     });
     this.getData();
   }
+
   // convenience getter for easy access to form fields
   get f() { return this.registerForm.controls; }
+
   onSubmit() {
     this.submitted = true;
-    let adminProfile = {
-      id: this.registerForm.controls['id'].value,
-      name: this.registerForm.controls['name'].value,
-      sal: this.registerForm.controls['sal'].value,
-      phone: this.registerForm.controls['phone'].value,
-      city: this.registerForm.controls['city'].value,
+
+    let empProfile = {
+      "employmentDetails": [{
+        empId: this.registerForm.controls['empId'].value,
+        empName: this.registerForm.controls['empName'].value,
+        empSal: this.registerForm.controls['empSal'].value,
+        mobileNumber: this.registerForm.controls['mobileNumber'].value,
+        addres: this.registerForm.controls['addres'].value,
+      }],
+      "transactionType": "save"
     }
-    console.log("555", adminProfile)
+    console.log("555", empProfile)
     // stop here if form is invalid
     if (this.registerForm.invalid) {
       return;
     }
-    this.http.post("http://192.168.3.183:8081/poc2/employee/save",adminProfile).subscribe((res) => {
+    this.http.post("http://192.168.3.238:7777/set",empProfile).subscribe((res) => {
       this.results = res;
       //this.myRoute.navigate(["/dashboard"]);
 
@@ -55,11 +61,59 @@ export class AllEmployeesComponent implements OnInit {
     this.getData();
   }
   getData() {
-    this.http.get("http://192.168.3.183:8081/poc2/employee/getAll/0/100").subscribe((res) => {
-      this.data = res;
+    let req = {
+      "employmentDetails": [{}],
+      "transactionType": "getAll"
+    }
+    this.http.post<any>('http://192.168.3.238:7777/get', req).subscribe(res => {
+      this.data = res['employmentDetailsList'];
       // this.studentDetails=this.results['data']
       console.log("111", this.data)
     })
+  }
+  empDelete(employesDeleteData) {
+    this.employeId = employesDeleteData.empId;
+    let delete_req = {
+      "employmentDetails": [{
+        "empId": this.employeId
+      }],
+      "transactionType": "delete"
+    }
+    this.http.post<any>('http://192.168.3.238:7777/set', delete_req).subscribe(res => {
+      this.data = res;
+      this.getData();
+    })
+  }
+  empEdit(employeEditData) {
+    this.registerForm.patchValue({
+      empId: employeEditData.empId,
+      empName: employeEditData.empName,
+      empSal: employeEditData.empSal,
+      mobileNumber: employeEditData.mobileNumber,
+      addres: employeEditData.addres
+    });
+    
+      
+
+  }
+  empUpadate(){
+    this.submitted = true;
+    let empEditform = {
+      "employmentDetails":[{
+      'empId': this.registerForm.controls['empId'].value,
+      'empName': this.registerForm.controls['empName'].value,
+      'empSal': this.registerForm.controls['empSal'].value,
+      'mobileNumber': this.registerForm.controls['mobileNumber'].value,
+      'addres': this.registerForm.controls['addres'].value,
+    }],
+    "transactionType":"update"
+    }
+      return this.http.post<any>('http://192.168.3.238:7777/set',empEditform).subscribe(res => {
+        this.data = res;
+        console.log("33",this.data);
+        
+        this.getData();
+      })
   }
 
 }
